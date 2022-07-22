682f0c32ff5b46805e8c1187381e8d62eb29a0f1
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2022-07-23 00:06:45 +0530

    creat Insertion part

--- Doubly_Link_list.cpp
+++ Doubly_Link_list.cpp
@@ -24,34 +24,85 @@ struct node *head = NULL;
 
 int main(){
     int opt = 0;
-    while(opt != 3){
-        cout<<"1 : Display \n 2 : Insert at Beginning \n 3 : Exit"<<endl;
+    while(opt != 8){
+        cout<<" 1 : Display \n 2 : Insert at Beginning \n 8 : Exit"<<endl;
         cout<<"Enter your chooice : ";
         cin>> opt;
         
         switch (opt)
         {
-        case 1:
-        {
-            display();
-            break;
-        }
-        case 2:
-        {
-            InsertAtBeginning();
-            break;   
-        }
-        case 3:
-        {
-            cout<<"Exiting";
-            break;
-        }
-        default:
-        {
-            cout<<"Enter a correct chooice "<<endl;
-            break;
-        }
+            case 1:
+            {
+                display();
+                break;
+            }
+            case 2:
+            {
+                InsertAtBeginning();
+                break;   
+            }
+            case 8:
+            {
+                cout<<"Exiting";
+                break;
+            }
+            default:
+            {
+                cout<<"Enter a correct chooice "<<endl;
+                break;
+            }
         };
     }
     return 0; 
+}
+
+//Insertion
+
+int InsertAtBeginning(){
+    int value;
+    struct node *newnode = new node;
+
+    if(newnode == NULL){
+        cout<<"No Space"<<endl;
+    }
+    else{
+        cout<<"Enter the value : ";
+        cin>>value;
+
+        newnode->data = value;
+        newnode->previous = NULL;
+        
+        if(head == NULL){
+            newnode->next = NULL;
+            head = newnode;
+        }
+        else{
+            head->previous = newnode;
+            newnode->next = head;
+            head = newnode;
+        }
+        cout<<"Element Inserted"<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+//Display
+
+int display(){
+    struct node *temp = head;
+    if(head == NULL){
+        cout<<"Empty List"<<endl;
+    }
+    else{
+        cout<<"Elements in the list : "<<endl;
+        while(temp != NULL){
+            cout<<temp->data<<endl;
+            temp = temp->next;
+        }
+    }
+    return 0;
 }
