8a12381fff058d848e3e46bc8248fa2625f91ad9
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2022-07-22 13:36:24 +0530

    update main

--- Doubly_Link_list.cpp
+++ Doubly_Link_list.cpp
@@ -6,6 +6,9 @@ using namespace std;
 // Proto type
 
 int InsertAtBeginning();
+int display();
+
+
 
 // Node Structure
 
@@ -14,3 +17,41 @@ struct node{
     struct node *next;
     struct node *previous;
 };
+
+struct node *head = NULL;
+
+// Main Function 
+
+int main(){
+    int opt = 0;
+    while(opt != 3){
+        cout<<"1 : Display \n 2 : Insert at Beginning \n 3 : Exit"<<endl;
+        cout<<"Enter your chooice : ";
+        cin>> opt;
+        
+        switch (opt)
+        {
+        case 1:
+        {
+            display();
+            break;
+        }
+        case 2:
+        {
+            InsertAtBeginning();
+            break;   
+        }
+        case 3:
+        {
+            cout<<"Exiting";
+            break;
+        }
+        default:
+        {
+            cout<<"Enter a correct chooice "<<endl;
+            break;
+        }
+        };
+    }
+    return 0; 
+}
