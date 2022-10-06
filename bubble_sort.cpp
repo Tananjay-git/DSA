e31e2a1fa4625cc5a37b11c33cc7b93a28623b38
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2022-10-06 09:43:28 +0530

    update

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -23,7 +23,55 @@ int insert(){
         new_node->data = value;
 
         if(head == NULL){
+            new_node->next = NULL;
+            head = new_node;
+        }
+        else{
+            new_node->next = head;
+            head = new_node;
+        }
+        cout<<"Element inserted"<<endl;
+    }
+    return 0;
+}
 
+void show(){
+    if(head == NULL){
+        cout<<"Empty list"<<endl;
+    }
+    else{
+        struct node *p = head;
+        cout<<"Element in List : ";
+        while(p->next != NULL){
+            cout<< p->data<< " ";
+            p=p->next;
         }
     }
+}
+
+
+int main(){
+    int opt = 0;
+    while(opt != 3){
+        cout<<"Enter Your Choice : "<<endl;
+        cout<<"1 : Insert \n2 : Show \n3 : Exit"<<endl;
+        cin>>opt;
+
+        switch (opt)
+        {
+            case 1:{
+                insert();
+                break;
+            }
+            case 2:{
+                show();
+                break;
+            }
+            case 3:{
+                cout<<"Exit";
+                break;
+            }
+        }
+    }
+    return 0;
 }
