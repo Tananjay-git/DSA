c70a5941df800375ed28555312ffe11423156b5c
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2024-04-07 18:46:08 +0530

    adding push function

--- linkedList.js
+++ linkedList.js
@@ -10,9 +10,25 @@ class linkedList{
         const newNode = new node(value);
         this.head = newNode
         this.tail = newNode
+        this.length = 1 
+    }
+
+    push(value){
+        const newNode = new node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }
+        else{
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this 
     }
 }
 
 let myLinkedList = new linkedList(5);
+myLinkedList.push(4)
 
 console.log(myLinkedList);
